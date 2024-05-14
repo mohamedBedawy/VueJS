ee17b5da87657e9551bd6825eefcef1d8d0bf787
Mohamed Bedawy <mohamed.bedawy@maqta.ae>
2024-05-14 15:18:34 +0400

    create Welcome message component

--- elzero_training/src/App.vue
+++ elzero_training/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WelcomeMessage msg="Welcome Bedawy"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WelcomeMessage from "@/components/welcomeMessage.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      WelcomeMessage,
   }
 }
 </script>
