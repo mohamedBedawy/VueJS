353debb1062311b82c5e3e89432c1c5d4a913ada
Mohamed Bedawy <mohamed.bedawy@maqta.ae>
2024-07-18 14:46:59 +0400

    Add new Project gRPC

---
+++ grpc-project/src/components/Greeting.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  div
+    h2
+      | gRPC Web with Vue 3
+
+</template>
+
+<script>
+export default {
+  name: `Greeting`,
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+  created() {
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ grpc-project/src/plugins/vuetify.js
@@ -0,0 +1,6 @@
+// src/plugins/vuetify.js
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles' // Import Vuetify styles
+
+// Pass in any Vuetify configuration options here
+export default createVuetify()
